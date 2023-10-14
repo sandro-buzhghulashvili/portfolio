98d3a4fc5dfbc0387052ab383b5324b10728117f
sandro <sbuzhghulashvili@gmail.com>
2023-10-14 19:44:01 +0400

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import classes from './Navbar.module.scss';
 
 const Navbar = () => {
+  const [toggleNavbar,setToggleNavbar] = useState(false)
+
+  const toggleNavbarHandler = () => {
+    setToggleNavbar(prevValue => !prevValue)
+  }
+
   return (
     <div className={classes.navbar}>
       <h1 className={classes.header}>Sandro.dev</h1>
-      <span className={classes.menu}></span>
-      <ul>
-        <li>
+      <span onClick={toggleNavbarHandler} className={`${classes.menu} ${toggleNavbar ? classes.close : undefined}`}></span>
+      <ul className={toggleNavbar ? classes.toggle : undefined}>
+        <li onClick={toggleNavbarHandler}>
           <a href="#">Home</a>
         </li>
-        <li>
+        <li onClick={toggleNavbarHandler}>
           <a href="#">About</a>
         </li>
-        <li>
+        <li onClick={toggleNavbarHandler}>
           <a href="#">Projects</a>
         </li>
-        <li>
+        <li onClick={toggleNavbarHandler}>
           <a href="#">Contact</a>
         </li>
       </ul>
