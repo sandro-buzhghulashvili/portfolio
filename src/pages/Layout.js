0bae5b450ea0ddfb61ac10b4b98827a810ed1088
sandro <sbuzhghulashvili@gmail.com>
2023-11-02 19:07:58 +0400

    added custom cursor

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import CustomCursor from '../components/UI/CustomCursor';
 
 const LayoutPage = () => {
+  const [cursorPosition,setCursorPosition] = useState({x : "50%", y : "50%"})
+
+  const cursorHandler = (event) => {
+    setCursorPosition({
+      x : event.clientX,
+      y : event.clientY
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", cursorHandler)
+
+    return () => {
+      window.removeEventListener("mousemove", cursorHandler)
+    }
+  }, [])
   return (
     <>
+      <CustomCursor posX={cursorPosition.x} posY={cursorPosition.y}/>
       <Navbar />
       <Outlet />
       <Footer />
