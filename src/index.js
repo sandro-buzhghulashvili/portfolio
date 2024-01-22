bfe67c88cbe69953be270a1eee61524b9bad812e
sandro <sbuzhghulashvili@gmail.com>
2024-01-22 23:47:21 +0400

    modified hero section

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.scss";
+import App from "./App";
+import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,30 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import CustomCursor from '../components/UI/CustomCursor';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const LayoutPage = () => {
-  const [cursorPosition,setCursorPosition] = useState({x : "50%", y : "50%"})
-
-  const cursorHandler = (event) => {
-    setCursorPosition({
-      x : event.clientX,
-      y : event.clientY
-    })
-  }
-
-  useEffect(() => {
-    window.addEventListener("mousemove", cursorHandler)
-
-    return () => {
-      window.removeEventListener("mousemove", cursorHandler)
-    }
-  }, [])
   return (
     <>
-      <CustomCursor posX={cursorPosition.x} posY={cursorPosition.y}/>
       <Navbar />
       <Outlet />
       <Footer />
